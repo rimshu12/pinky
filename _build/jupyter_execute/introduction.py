18c4a42303b943d583629356212ba1d85eca1fb0
rimshu12 <rimshuwahid12@gmail.com>
2022-04-13 16:38:53 +0800

    adding my book!

---
+++ _build/jupyter_execute/introduction.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+
+# This jupyter book can help you understand more about mircobit compass and sunlight sensors.You can go through them all to develop your understanding of using the micro:bit’s sensors. This book include different codes and videos to help doing your micro:bit compass and sunlight sensors. 
+# 
+# Coding with the micro:bit encourages you to gain basic skills preparing you for today’s modern digital world. The BBC micro:bit integrates the code you write with the embedded hardware to create interesting projects, while teaching you how to control hardware through programming. This promotes an understanding within kids by demonstrating they have to program the hardware to respond to their code and input.
+
+# # Micro:bit
+
+# The BBC micro:bit is a pocket-sized computer that introduces you to how software and hardware work together. It has an LED light display, buttons, sensors and many input/output features that you can program and physically interact with. The latest micro:bit adds sound sensing and playback capabilities.
+
+# # Sensors
+
+# Building on your learning in the LEDs and buttons sets, these projects and videos will show you how to program your micro:bit to sense movement, light, temperature and magnetic fields.
+
+#  # What is Microbit compass 
+
+# micro:bit has a compass sensor called a magnetometer that measures magnetic fields. It can sense the Earth's magnetic field and so you can use it as a compass. The micro:bit measures the compass heading from 0 to 359 degrees with its magnetometer chip. Different numbers mean north, east, south, and west.
+# 
+# The micro: bit display letters or arrows to show if it’s pointing North, South, East or West. the micro: bit takes a reading from the compass sensor and shows the device’s numerical compass bearing on the LED display output
+
+# # what is sunglight sensor
+
+# The micro:bit measures the light around it by using some of the LEDs on the LED screen. The first time you use it, this function will say 0 . After that, it will say the real light level. This is because the light sensor (the part that can find the light level) has to be turned on first. light level. Show a different picture, like a moon or star. 
+
+# In[ ]:
+
+
+
+
