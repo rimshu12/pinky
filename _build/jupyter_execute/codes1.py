4f35b7854f36066165c2354b6bfa776159478214
rimshu12 <rimshuwahid12@gmail.com>
2022-04-25 17:20:42 +0800

    adding my book!

---
+++ _build/jupyter_execute/codes1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython import display
+display.HTML('<iframe src="https://python.microbit.org/" allow="usb;serial"  width=800 height=500></iframe>')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
